9f7667c5de6f0d1e31ec849eb87c395f4bf1d7cf
Vincent Chataignier <vincent.chataignier@etu.univ-tours.fr>
2015-04-10 16:20:35 +0200

    auththth

--- mysql.js
+++ mysql.js
@@ -104,7 +104,7 @@ app.post("/authenticate",function(req,res){
                 res.end();
             }
             else {
-                user = ('"user":['+JSON.stringify(rows[0])+']');
+                user = ('"user":'+JSON.stringify(rows[0])+'');
                 console.log(user);
             }
         });
@@ -121,8 +121,8 @@ app.post("/authenticate",function(req,res){
                 res.setHeader('Content-Type', 'application/json');
                 res.writeHead(200);
 
-                token = '["token":'+JSON.stringify(rows[0].token)+',';
-                retour = '{"auth":'+token + user+']}';
+                token = '{"token":'+JSON.stringify(rows[0].token)+',';
+                retour = '{"auth":'+token + user+'}}';
                 res.end(retour);
             }
         });
